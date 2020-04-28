c0851652bdaa189a1105afabc4b61edf01a067c7
Ali Cy <califynic@gmail.com>
2020-04-28 00:55:09 -0700

    Made random sampling O(m) instead of O(m log m)

--- algos/randompermute.java
+++ algos/randompermute.java
@@ -21,28 +21,26 @@ public class randompermute {
     return arr;
   }
 
-  static ArrayList<Integer> randomsample_g(int m, int n){
+  static HashMap<Integer, Integer> randomsample_g(int m, int n){
     if(m == 0){
-      return new ArrayList<Integer>();
+      return new HashMap<Integer, Integer>();
     } else {
-      ArrayList<Integer> S = randomsample_g(m - 1, n - 1);
+      HashMap<Integer, Integer> S = randomsample_g(m - 1, n - 1);
       int i = interrandom(0, n);
-      int j = Collections.binarySearch(S, i);
-      if(j < 0){
-        j = -1 * j - 1;
-        S.add(j, i);
+      if(S.containsKey(i)){
+        S.put(n - 1, -1);
       } else {
-        S.add(n);
+        S.put(i, -1);
       }
       return S;
     }
   }
 
   public static int[] randomsample(int[] arr, int m){
-    ArrayList<Integer> indices = randomsample_g(m, arr.length);
+    Object[] indices = randomsample_g(m, arr.length).keySet().toArray();
     int[] out = new int[m];
     for(int i = 0; i < m; i++){
-      out[i] = arr[indices.get(i)];
+      out[i] = arr[(Integer) indices[i]];
     }
     out = randompermutation(out);
     return out;
@@ -51,15 +49,15 @@ public class randompermute {
   public static void main(String[] args){
     // Examples of usage
     int[] arr = new int[]{3, 1, 4, 2, 5, 3};
-    //System.out.println("Original array: " + Arrays.toString(arr));
+    System.out.println("Original array: " + Arrays.toString(arr));
 
     // Random permutation of arr
     arr = randompermutation(arr);
-    //System.out.println("After random permutation: " + Arrays.toString(arr));
+    System.out.println("After random permutation: " + Arrays.toString(arr));
 
     // Random sample of size 2 from arr
     int[] sample = randomsample(arr, 2);
-    //System.out.println("A random sample: " + Arrays.toString(sample));
+    System.out.println("A random sample: " + Arrays.toString(sample));
   }
 
 
