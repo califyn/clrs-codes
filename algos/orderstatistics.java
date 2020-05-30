8d5349f3ebd533f64b73fe2616b4b0bc3ba2b43e
Ali Cy <califynic@gmail.com>
2020-05-30 00:24:01 -0700

    Added second order statistics algorithm

--- algos/orderstatistics.java
+++ algos/orderstatistics.java
@@ -68,13 +68,138 @@ public class orderstatistics {
     return select_g(arr, 0, arr.length, i);
   }
 
-  static int Select(int[] arr, )
+  static String ArrayListString(ArrayList<Integer> arr){
+    if(arr.size() == 0){
+      return "[]";
+    }
+    String s = "[";
+    for(int i = 0; i < arr.size(); i++){
+      s = s + Integer.toString(arr.get(i)) + ", ";
+    }
+    s = s.substring(0, s.length() - 2);
+    s = s + "]";
+    return s;
+  }
+
+  static int[] bubble_sort(int[] arr){
+    int bubbles = 1;
+    do {
+      bubbles = 0;
+      for (int i = 0; i < arr.length - 1; i++){
+        if(arr[i + 1] < arr[i]){
+          swap(arr, i, i + 1);
+          bubbles++;
+        }
+      }
+    } while (bubbles > 0);
+    return arr;
+  }
+
+  static int S_partition(int[] arr, int pow, int p, int r, int k, int c){
+    int val = arr[k];
+    swap(arr, k, c);
+    int i = (int) Math.floor(2 * (Math.pow(5, pow) - 1) / 4) + p;
+    int pairOff = 0;
+    for(int j = (int) Math.floor(2 * (Math.pow(5, pow) - 1) / 4) + p; j < c; j += Math.pow(5, pow)){
+      if(arr[j] < val){
+        swap(arr, j, i);
+        i += Math.pow(5, pow);
+        if(i >= r - r % (int) Math.floor(Math.pow(5, pow))){
+          i = c;
+        }
+      }
+    }
+    if(arr[c] < val){
+      swap(arr, c, i);
+      i += Math.pow(5, pow);
+      if(i >= r - r % (int) Math.floor(Math.pow(5, pow))){
+        i = c;
+      }
+    }
+    swap(arr, i, c);
+    return i;
+  }
+
+  static int Select_g(int[] arr, int pow, int p, int r, int k, int c){
+    if (r - p <= Math.pow(5, pow + 1)){
+      ArrayList<Integer> indices = new ArrayList<Integer>();
+      int l = p + (int) Math.floor(Math.pow(5, pow));
+      while (l <= r) {
+        indices.add(l - (int) Math.floor((Math.pow(5, pow) + 1) / 2));
+        l += (int) Math.floor(Math.pow(5, pow));
+      }
+      if (c != -1){
+        indices.add(c);
+      }
+      Collections.sort(indices, new Comparator<Integer>(){
+        public int compare(Integer left, Integer right) {
+          return arr[left] - arr[right];
+        }
+      });
+      return indices.get(k);
+    }
+    for (int i = (int) Math.floor(2 * (Math.pow(5, pow + 1) - 1) / 4) + p; i < r - (int) Math.floor(2 * Math.pow(5, pow)); i += Math.pow(5, pow + 1)) {
+      int[] sorted = new int[]{i - (int) Math.floor(2 * Math.pow(5, pow)), i - (int) Math.floor(1 * Math.pow(5, pow)), i, i + (int) Math.floor(1 * Math.pow(5, pow)), i + (int) Math.floor(2 * Math.pow(5, pow))};
+      for (int j = 0; j < 5; j++){
+        sorted[j] = arr[sorted[j]];
+      }
+      sorted = bubble_sort(sorted);
+      for (int j = -2; j < 3; j++){
+        arr[i + j * (int) Math.floor(Math.pow(5, pow))] = sorted[j + 2];
+      }
+    }
+    int l = (int) Math.floor((r - p) / Math.pow(5, pow + 1));
+    l *= Math.pow(5, pow + 1);
+    l += p;
+    l += Math.pow(5, pow);
+    ArrayList<Integer> indices = new ArrayList<Integer>();
+    while (l - (int) Math.floor((Math.pow(5, pow) + 1) / 2) < r) {
+      indices.add(l - (int) Math.floor((Math.pow(5, pow) + 1) / 2));
+      l += Math.pow(5, pow);
+    }
+    if (c != -1) {
+      indices.add(c);
+    }
+    int[] sorted = new int[indices.size()];
+    for (int i = 0; i < sorted.length; i++){
+      sorted[i] = arr[indices.get(i)];
+    }
+    sorted = bubble_sort(sorted);
+    for (int i = 0; i < sorted.length; i++){
+      arr[indices.get(i)] = sorted[i];
+    }
+    int index;
+    if (Math.ceil((r - p)/Math.pow(5, pow)) > 5){
+      index = Select_g(arr, pow + 1, p, r, (int) Math.floor(Math.floor(Math.ceil((r - p) / Math.pow(5, pow + 1)) - 1)/2), indices.size() == 0 ? -1 : indices.get((indices.size() - 1)/2));
+    } else {
+      index = (int) Math.floor((Math.ceil((r - p)/Math.pow(5, pow)) -1)/2);
+    }
+    int t = S_partition(arr, pow, p, r, index, c == -1 ? r- 1 : c);
+    int s = (int) Math.floor((t - p) / Math.pow(5, pow));
+    if (s == k){
+      return t;
+    } else if (s > k){
+      if (t == c) {
+        return Select_g(arr, pow, p, (int) Math.floor(((int) ((r - p) / Math.pow(5, pow))) * Math.pow(5, pow)) + p, k, -1);
+      }
+      return Select_g(arr, pow, p, t, k, -1);
+    } else {
+      return Select_g(arr, pow, (int) Math.floor((s + 1) * Math.pow(5, pow)) + p, r, k - s - 1, c);
+    }
+  }
+
+  static int Select(int[] arr, int k){
+    int ret = Select_g(arr, 0, 0, arr.length, k, -1);
+    ret = arr[ret];
+    return ret;
+  }
 
   public static void main(String[] args){
     int[] arr = new int[]{3, 1, 5, 2, 4, 3};
     /*
     for(int i = 0; i < 6; i++){
       arr = new int[]{3, 1, 5, 2, 4, 3};
+      System.out.println(i + "th element: " + Select(arr, i));
       System.out.println(i + "th element: " + select(arr, i));
     }
     */
